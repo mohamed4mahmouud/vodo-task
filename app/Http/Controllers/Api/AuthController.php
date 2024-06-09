f01b9cc9487d90361604fa403d4821b047811e0d
mohamed4mahmouud <mohamedalamrawy55@gmail.com>
2024-06-09 03:07:47 +0300

    handle login function

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -31,8 +33,36 @@ class AuthController extends Controller
             return response()->json([
                 'status' => false,
                 'message' => $th->getMessage()
-            ]);
+            ],500);
         }
 
+    }
+
+    public function login (LoginRequest $request)
+    {
+        try {
+
+            if(!Auth::attempt($request->only(['username','password']))){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Incorrect username or password'
+                ],401);
+            }
+
+            $user = User::where('username' , $request->username)->first();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User logged in successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ],200);
+
+        } catch (\Throwable $th) {
+
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ],500);
+        }
     }
 }
